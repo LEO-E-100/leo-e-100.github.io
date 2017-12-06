0d95402cc1ba789241850d7fe59bddd7d80186fe
Leo Edwards <thejpe100@gmail.com>
2017-12-06 18:46:11 +0000

    Total Re-Write

--- _posts/2017-06-12-Rugby-Union-Analysis.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-title: "r/RugbyUnion Player Ratings"
-date: 2017-06-12
----
-
-### [Code Repo](https://github.com/LEO-E-100/Lions_Tour_Ratings)
---
+++ scripts/html_converter.py
@@ -0,0 +1,24 @@
+import sys
+from nbconvert import HTMLExporter
+import nbformat
+
+notebook_file = sys.argv[1]
+html_exporter = HTMLExporter()
+nb = nbformat.reads(open(notebook_file, 'r').read(), as_version=4)
+
+(body, resources) = html_exporter.from_notebook_node(nb)
+
+
+read_navbar = open("navbar.txt", 'r').read()
+read_css = open("main_css.txt", 'r').read()
+
+if read_navbar not in body:
+    body = body.replace("<body>", "<body>\n" + read_navbar)
+
+if read_css not in body:
+    body = body.replace("</title>", "</title>\n" + read_css)
+
+html_file = notebook_file.replace(".ipynb", ".html")
+html_file_writer = open(html_file, 'w')
+html_file_writer.write(body)
+html_file_writer.close()
